30d2daffb0b1b87cc60a6d8a8df27678c61e6898
Lucas Cruz <lucas.apcruz@gmail.com>
2022-12-01 18:04:27 -0300

    feat: vote on choice

--- src/controllers/choices.controller.js
+++ src/controllers/choices.controller.js
@@ -1,4 +1,5 @@
-import { choices } from "../database/db.js"
+import { choices, votes } from "../database/db.js"
+import { formatDateTime } from "../library/miscellanous.js";
 
 export async function defineChoice(req, res){
     const choice = req.body
@@ -6,6 +7,25 @@ export async function defineChoice(req, res){
     try {
         await choices
             .insertOne(choice)
+
+        res.sendStatus(201)
+
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+export async function voteOnChoice(req, res){
+    const pollId = req.params.id
+    const vote = {
+        createdAt: formatDateTime(Date.now()),
+        pollId
+    }
+
+
+    try {
+        await votes
+            .insertOne(vote)
 
         res.sendStatus(201)
 
--- src/library/miscellanous.js
+++ src/library/miscellanous.js
@@ -3,3 +3,7 @@ export const dateTimeFormat = "YYYY-MM-DD HH:mm"
 export function daysToSeconds(days){
     return days*24*60*60*1000
 }
+
+export function formatDateTime(dateTimeMills){
+    return dayjs(dateTimeMills).format(dateTimeFormat)
+}
