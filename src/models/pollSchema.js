6aa0717343a70637cebfc78a595cdaba59f4803d
Lucas Cruz <lucas.apcruz@gmail.com>
2022-12-02 10:56:24 -0300

    feat: poll expireAt date valdation

--- src/models/pollSchema.js
+++ src/models/pollSchema.js
@@ -1,6 +1,8 @@
-import joi from "joi"
+import baseJoi from "joi"
+import dateExtension from '@joi/date';
+const joi = baseJoi.extend(dateExtension)
 
 export const pollSchema = joi.object({
     title: joi.string().required().min(1),
-    expireAt: joi.required()
+    expireAt: joi.date().format("YYYY-MM-DD HH:mm")
 })
